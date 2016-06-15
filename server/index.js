aed9132b0954c27d9a73bbbb6ecb6c993c3e6b45
Aaron Flower <aaronpflower@gmail.com>
2016-06-15 14:06:03 -0600

    stuff

--- server/index.js
+++ server/index.js
@@ -9,8 +9,16 @@ app.get('/', function (req, res) {
 
 io.on('connection', function(socket) {
 	console.log("YO YOU CONNECTED");
-  socket.on("chat message", function(msg){
-    console.log(msg)
+	io.emit("chat message", "New User Connected")
+	socket.on('disconnect', function() {
+		io.emit("chat message", "User Disconnected")
+	})
+  socket.on("chat message", function(obj){
+  	io.emit('chat message', { "msg": obj.msg, "username": obj.username } )
+    console.log(obj)
+  })
+  socket.on("user typing", function(username) {
+  	socket.broadcast.emit('user typing', username)
   })
 })
 
