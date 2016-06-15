cb11221a71d6dec4f921fd2363fe303ebdc1cf0c
Aaron Flower <aaronpflower@gmail.com>
2016-06-15 11:10:15 -0600

    added stuff

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
+io.on('connection', function(socket) {
+	console.log("YO YOU CONNECTED");
+})
+
+http.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
